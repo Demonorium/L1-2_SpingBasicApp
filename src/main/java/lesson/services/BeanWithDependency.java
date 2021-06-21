e5afb8ddb4bd2f2902bb2b050a3a8bc20af805a0
Demonorium <Demonorium@yandex.ru>
2021-06-21 14:42:11 +0300

    Update

--- src/main/java/lesson/services/BeanWithDependency.java
+++ src/main/java/lesson/services/BeanWithDependency.java
@@ -1,7 +1,14 @@
 package lesson.services;
 
 
-public class BeanWithDependency {
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+
+public class BeanWithDependency implements ApplicationListener {
+    private static final Logger logger = LogManager.getLogger(BeanWithDependency.class);
+
     private GreetingService greetingService;
     public BeanWithDependency(GreetingService greetingService) {
         this.greetingService = greetingService;
@@ -9,5 +16,14 @@ public class BeanWithDependency {
 
     public String sayGreeting() {
         return greetingService.sayGreeting() + " It's long!";
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationEvent applicationEvent) {
+        if (applicationEvent instanceof BlackListEvent) {
+            BlackListEvent event = (BlackListEvent) applicationEvent;
+            logger.info(event.getAddress());
+            logger.info(event.getTest());
+        }
     }
 }
